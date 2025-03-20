56c3601366e54c6cb177e99b67aa57e098f7ffa0
mesel7 <cejhans1520@gmail.com>
2025-03-21 03:58:50 +0900

    feat: React Router 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Navbar from './components/common/NavBar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   )
 }
