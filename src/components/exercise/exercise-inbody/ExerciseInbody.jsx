5796a0cf80c6eed1932754c02cf6472ba366ee08
mesel7 <cejhans1520@gmail.com>
2025-04-29 16:29:22 +0900

    feat: ExerciseInbody UI 구현

--- src/components/exercise/exercise-inbody/ExerciseInbody.jsx
+++ src/components/exercise/exercise-inbody/ExerciseInbody.jsx
@@ -1,8 +1,22 @@
 import "./ExerciseInbody.css";
+import inbodyLogo from "../../../assets/images/inbody-logo.png";
+import sampleInbody from "../../../assets/images/sample-inbody.png"
+import DateSelector from "../../common/date-selector/DateSelector";
 
 const ExerciseInbody = () => {
     return (
-        <div className="exercise-inbody"></div>
+        <div className="exercise-inbody">
+            <div className="exercise-inbody__header">
+                <div className="exercise-inbody__title">
+                    <img src={inbodyLogo} alt={"INBODY"} />
+                    <div className="excise-inbody__file-view">
+                        {"파일로 보기 >"}
+                    </div>
+                </div>
+                <DateSelector />
+            </div>
+            <img className={"exercise-inbody__data"} src={sampleInbody} alt={"INBODY-DATA"} />
+        </div>
     );
 };
 
